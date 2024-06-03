a7ae37911f893be68d60c35f0680c7ead4a97b08
thuylinh5672004 <164979311+thuylinh5672004@users.noreply.github.com>
2024-06-03 11:24:49 +0700

    li

---
+++ DTTLBaiDanhGiaGiuaky/DTTLBaiDanhGiaGiuaky/Controllers/DTLProductController.cs
@@ -0,0 +1,44 @@
+﻿using DTTLBaiDanhGiaGiuaky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DTTLBaiDanhGiaGiuaky.Controllers
+{
+    public class DTLProductController : Controller
+    {
+        private static List<DTLProduct> dTLProducts = new List<DTLProduct>()
+
+        {
+            new DTLProduct{ID = 221090036,dtlFullName="thùy linh", dtlImage="anh",  dtlQuantity=23,dtlPrice =12000, dtlisActive='8'},
+            new DTLProduct{ID = 221090037,dtlFullName="thùy thảo", dtlImage="anh",  dtlQuantity=27,dtlPrice =15000, dtlisActive='6'},
+
+        };
+        // GET: DTL
+
+
+        public ActionResult Index()
+        {
+            return View(dTLProducts);
+        }
+        public ActionResult DTLCreate()
+        {
+            var dtlProduct = new DTLProduct();
+            return View(dtlProduct);
+        }
+        // GET: DTL
+        [HttpPost]
+        public ActionResult DTLCreate(DTLProduct dtlProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dtlProduct);
+            }
+            //nếu dữ liệu đúng thì lưu vào danh sách
+            dTLProducts.Add(dtlProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DTTLBaiDanhGiaGiuaky/DTTLBaiDanhGiaGiuaky/Models/DTLProduct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace DTTLBaiDanhGiaGiuaky.Models
+{
+    public class DTLProduct
+    {
+        public int ID { get; set; }
+        [DisplayName("DTL ID")]
+        [Required(ErrorMessage = "DTL: Chưa nhập dữ liệu")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "DTL: Họ tên chứa tối thiểu 2 ký tự tối đa 25")]
+        [RegularExpression(@"[A-Za-z]+", ErrorMessage = "DTL: Họ tên chỉ chưa các ký tự hoặc khoảng trắng")]
+        public string dtlFullName { get; set; }
+        [DisplayName("DTL Họ và tên")]
+        [Required(ErrorMessage = "DTL: Chưa nhập dữ liệu")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "DTL: Họ tên chứa tối thiểu 2 ký tự tối đa 25")]
+        [RegularExpression(@"[A-Za-z]+", ErrorMessage = "DTL: Họ tên chỉ chưa các ký tự hoặc khoảng trắng")]
+        public string dtlImage { get; set; }
+        [DisplayName("DTL: ảnh")]
+        [Required(ErrorMessage = ": Chưa nhập dữ liệu")]
+        
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "")]
+        public int  dtlQuantity { get; set; }
+        [DisplayName("DTL nhập số lượng")]
+        [Required(ErrorMessage = "DTL: Hãy nhập số lượng")]
+        [RegularExpression(@"[1-100]{2}", ErrorMessage = "DTL: Hãy nhập 2 ký tự từ số")]
+        [Range(1, 100, ErrorMessage = "DTL: Hãy nhập giá trị trong khoảng [1-100]")]
+        public int dtlPrice { get; set; }
+        [DisplayName("DTL nhập giá")]
+        [Required(ErrorMessage = "DTL: Hãy nhập giá tiền")]
+        [RegularExpression(@"\d+\.\d+", ErrorMessage = "DTL: Hãy nhập giá tiền ")]
+        [Range(1, 100, ErrorMessage = "DTL: giá tiền của sản phẩm ")]
+        public int dtlisActive { get; set; }
+
+
+    }
+}

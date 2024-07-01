6b5c3cf45aab6ea2e28d65570b20d07a9cb91ecb
thuylinh5672004 <164979311+thuylinh5672004@users.noreply.github.com>
2024-07-01 23:24:23 +0700

    less11

---
+++ DtlK22CNT1Lesson11/DtlK22CNT1Lesson11/Controllers/DtlHomeController.cs
@@ -0,0 +1,37 @@
+﻿using DtlK22CNT1Lesson11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DtlK22CNT1Lesson11.Controllers
+{
+    public class DtlHomeController : Controller
+    {
+        public ActionResult DtlIndex()
+        {
+            // lấy thông tin từ session
+            //ViewBag["tvcTaiKhoan"] = "";
+            if (Session["DtlMember"] != null)
+            {
+                ViewBag.dtlTaiKhoan = ((DtlTaiKhoan)Session["DtlMember"]).DtlFullName;
+            }
+            return View();
+        }
+
+        public ActionResult DtlAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult DtlContact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DtlK22CNT1Lesson11/DtlK22CNT1Lesson11/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DtlK22CNT1Lesson11.MvcApplication" Language="C#" %>
